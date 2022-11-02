d9c6041ef7b470f9f3169206ef1d82e1ab49e36e
Aleksandra Zolushkina <aleksandra.zolushkina@jetbrains.com>
2022-11-02 11:35:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers:");
+
+        int first = args[0];
+
+        System.out.println("First number is: " + first);
+
+        int second;
+
+        Integer.parseInt(args[1]);
+
+        System.out.println("Second numver is: " + second);
+
+        // add tow numbers
+        System.out.println("Adding two numbers");
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+
+    }
+}
+
