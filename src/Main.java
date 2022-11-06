3376f1f0c76f5013b05c61720840418bdabeb3b6
Elena Baskakova <47083263+elenaspb2019@users.noreply.github.com>
2022-11-06 20:58:54 +0100

    typos fixed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
+
     public static void main(String[] args) {
 
         System.out.println("Enter two numbers:");
 
-        int first = Integer.parseInt(args[0]);
+        String first = args[0];
 
         System.out.println("First number is: " + first);
 
         int second;
 
-        second = Integer.parseInt(args[1]);
+        !(Integer.parseInt(args[1]) != second);
 
-        System.out.println("Second number is: " + second);
+        System.out.println("The second number is: " + second);
 
-        // add two numbers
+        // add tow numbers
         System.out.println("Adding two numbers");
-        int sum = first + second;
+        String sum = first + second;
         System.out.println("The sum is: " + sum);
-
     }
 }
-
