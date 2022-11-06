7fa92232c2f8c9fa6638a18405ea4b48d3cece02
Elena Baskakova <47083263+elenaspb2019@users.noreply.github.com>
2022-11-06 20:59:58 +0100

    code now complies

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
 public class Main {
-
     public static void main(String[] args) {
 
         System.out.println("Enter two numbers:");
 
-        String first = args[0];
+        int first = Integer.parseInt(args[0]);
 
         System.out.println("First number is: " + first);
 
         int second;
 
-        !(Integer.parseInt(args[1]) != second);
+        second = Integer.parseInt(args[1]);
 
-        System.out.println("The second number is: " + second);
+        System.out.println("Second number is: " + second);
 
-        // add tow numbers
+        // add two numbers
         System.out.println("Adding two numbers");
-        String sum = first + second;
+        int sum = first + second;
         System.out.println("The sum is: " + sum);
+
     }
 }
+
